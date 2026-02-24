01d903dddd0d462b95bc7b289c8387d52a5b448b
TJ <kessingertj@gmail.com>
2026-02-23 22:46:33 -0700

    added time in min

--- src/main/java/com/contractpal/s3checksum/Main.java
+++ src/main/java/com/contractpal/s3checksum/Main.java
@@ -11,12 +11,15 @@ public class Main {
             }
         }
 
+        long start = System.currentTimeMillis();
         S3Verifier verifier = new S3Verifier(System.getenv("inputFilePath"), System.getenv("bucketName"), Region.of(System.getenv("region")), System.getenv("accessKey"), System.getenv("secretKey"));
         verifier.verifyAll();
         if(!verifier.exportResultToFile("output")){
             System.out.println("ERROR exporting results!");
             System.exit(1);
         }
+        double minutes = (System.currentTimeMillis() - start) / 60000.0;
+        System.out.printf("Completed in %.2f minutes%n", minutes);
         System.exit(0);
     }
 }
